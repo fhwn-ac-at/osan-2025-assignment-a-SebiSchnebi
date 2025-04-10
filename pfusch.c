4c85c1a8c5a7151cb3f5bd3bc183e42099965f00
Krammer Sebastian <211063@fhwn.ac.at>
2025-04-10 10:26:48 +0200

    getopt

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(void) {
-    printf("TEST\n");
+typedef struct command_line_arguments {
+	int i;
+	char const *s;
+	bool b;
+} cli_args;
 
+
+cli_args parse_command_line (int const argc, char *argv[])
+{
+	cli_args args = {0, NULL, false};
+	
+	int optgot = -1;
+	
+	do
+	{
+		optgot = getopt(argc, argv, "i:s:b");
+		printf("gotopt: %c --> %s\n", optgot, optarg);
+		switch (optgot)
+		{
+			case 'i':
+			args.i = atoi(optarg);
+			break;
+			case 's':
+			args.s = optarg;
+			break;
+			case 'b':
+			args.b = true;
+			break;
+			case '?':
+			printf("Usage: %s -i <number> -s <string>, -b\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}	
+		
+		
+	} while (optgot != -1);
+	
+	if (args.i <= 0 || strlen(args.s) < 5)
+	{
+		printf("Usage: %s -i <number> -s <string>, -b\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	
+	return args;
+}
+
+
+
+int main(int argc, char *argv[], char *envp[]) 
+{
+
+	cli_args const args = parse_command_line(argc, argv);
+	printf("\ni: %d, s: %s, b: %d\n", args.i, args.s, args.b);
+	
     return 0;
 }
